fec25f1b7bd174d3bcdf7dabdbfe74aa342518e3
Keith Isham <kisham8899@gmail.com>
2019-05-14 15:05:53 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,6 +2,35 @@ class Song
   attr_accessor :name, :artist_name
   @@all = []
 
+  def self.create
+    song = Song.new
+    song.save
+    song
+  end
+
+  def self.new_by_name(name)
+    song = Song.new
+    song.name = name
+    song
+  end
+
+  def self.create_by_name(name)
+    song = Song.create
+    song.name = name
+    song
+  end
+
+  def self.find_by_name(name)
+    @@all.each do |song|
+      if song.name == name
+        return song
+
+
+      end
+    end
+  end
+
+
   def self.all
     @@all
   end
