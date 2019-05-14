32a4170dab916e66d1e260a1f1a480c57c7cc710
Keith Isham <kisham8899@gmail.com>
2019-05-14 15:09:36 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -24,13 +24,7 @@ class Song
     @@all.each do |song|
       if song.name == name
         return song
-      else
-        song.name != name
-        return false
-      end
-
-
-
+    
     end
   end
 
